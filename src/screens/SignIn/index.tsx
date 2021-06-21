163a2f19921dfa3bf58bc5b1998cea90841ed24d
Anderson Moraes <anderson@ycloud.com.br>
2021-06-21 10:24:53 -0300

    Aula 1

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,42 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { 
+  Text, 
+  View,
+  Image,
+} from 'react-native';
+
+import { ButtonIcon } from '../../components/ButtonIcon';
+import IllustrationImg from '../../assets/illustration.png'
+import { styles } from './styles'
+
+export function SignIn() {
+  return (
+    <View style={styles.container}>
+      <StatusBar
+        backgroundColor="transparent"
+        translucent
+      />
+      <Image
+        source={ IllustrationImg } 
+        style={styles.image}
+        resizeMode="stretch"
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {`\n`}
+          suas jogatinas  {`\n`}
+          facilmente
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {`\n`}
+          favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon title="Entrar com Discord" activeOpacity={0.7} />
+      </View>
+    </View>
+  );
+}
