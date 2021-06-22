532154628f7d80e01f5be2bb801c677c62a9e50e
Anderson Moraes <anderson@ycloud.com.br>
2021-06-22 09:55:06 -0300

    Aula 2

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,22 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { 
   Text, 
   View,
   Image,
 } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { ButtonIcon } from '../../components/ButtonIcon';
 import IllustrationImg from '../../assets/illustration.png'
 import { styles } from './styles'
 
 export function SignIn() {
+  const navigation = useNavigation();
+
+  function handleSignIn() {
+    navigation.navigate('Home');
+  }
+
   return (
     <View style={styles.container}>
-      <StatusBar
-        backgroundColor="transparent"
-        translucent
-      />
       <Image
         source={ IllustrationImg } 
         style={styles.image}
@@ -25,17 +27,20 @@ export function SignIn() {
 
       <View style={styles.content}>
         <Text style={styles.title}>
-          Organize {`\n`}
-          suas jogatinas  {`\n`}
-          facilmente
+          Conecte-se {'\n'}
+          e organize suas  {'\n'}
+          jogatinas
         </Text>
 
         <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games {`\n`}
+          Crie grupos para jogar seus games {'\n'}
           favoritos com seus amigos
         </Text>
 
-        <ButtonIcon title="Entrar com Discord" activeOpacity={0.7} />
+        <ButtonIcon
+          title="Entrar com Discord" 
+          onPress={handleSignIn}
+        />
       </View>
     </View>
   );
