6c5d312ac9f104e2ced7b259d1f6436267e14574
Anderson Moraes <anderson@ycloud.com.br>
2021-06-23 09:27:41 -0300

    Aula 3

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,30 +1,41 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import { theme } from '../global/styles/theme';
+
 import { Home } from '../screens/Home';
 import { SignIn } from '../screens/SignIn';
+import { AppointmentDetails } from '../screens/AppointmentDetails';
+import { AppointmentCreate } from '../screens/AppointmentCreate';
 
 const { Navigator, Screen } = createStackNavigator();
 
 export function AuthRoutes() {
-  return (
+  return(
     <Navigator
       headerMode="none"
       screenOptions={{
         cardStyle: {
-          backgroundColor: 'transparent'
+          backgroundColor: theme.colors.secondary100
         }
       }}
     >
-      <Screen
+      <Screen 
         name="SignIn"
         component={SignIn}
       />
-      <Screen
+      <Screen 
         name="Home"
         component={Home}
       />
+      <Screen 
+        name="AppointmentDetails"
+        component={AppointmentDetails}
+      />
+      <Screen 
+        name="AppointmentCreate"
+        component={AppointmentCreate}
+      />      
     </Navigator>
-  );
+  )
 }
-
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -6,9 +6,11 @@ import {
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-import { ButtonIcon } from '../../components/ButtonIcon';
 import IllustrationImg from '../../assets/illustration.png'
 import { styles } from './styles'
+
+import { ButtonIcon } from '../../components/ButtonIcon';
+import { Background } from '../../components/Background';
 
 export function SignIn() {
   const navigation = useNavigation();
@@ -18,30 +20,32 @@ export function SignIn() {
   }
 
   return (
-    <View style={styles.container}>
-      <Image
-        source={ IllustrationImg } 
-        style={styles.image}
-        resizeMode="stretch"
-      />
-
-      <View style={styles.content}>
-        <Text style={styles.title}>
-          Conecte-se {'\n'}
-          e organize suas  {'\n'}
-          jogatinas
-        </Text>
-
-        <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games {'\n'}
-          favoritos com seus amigos
-        </Text>
-
-        <ButtonIcon
-          title="Entrar com Discord" 
-          onPress={handleSignIn}
+    <Background>
+      <View style={styles.container}>
+        <Image
+          source={ IllustrationImg } 
+          style={styles.image}
+          resizeMode="stretch"
         />
+
+        <View style={styles.content}>
+          <Text style={styles.title}>
+            Conecte-se {'\n'}
+            e organize suas  {'\n'}
+            jogatinas
+          </Text>
+
+          <Text style={styles.subtitle}>
+            Crie grupos para jogar seus games {'\n'}
+            favoritos com seus amigos
+          </Text>
+
+          <ButtonIcon
+            title="Entrar com Discord" 
+            onPress={handleSignIn}
+          />
+        </View>
       </View>
-    </View>
+    </Background>
   );
 }
