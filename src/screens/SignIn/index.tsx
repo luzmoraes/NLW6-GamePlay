1a886834716edafbd9a348e68ae891d3593c6567
Anderson Moraes <anderson@ycloud.com.br>
2021-06-27 20:45:16 -0300

    Aula 4/5

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,49 +1,60 @@
 import React from 'react';
 import { 
+  View, 
   Text, 
-  View,
   Image,
+  Alert,
+  ActivityIndicator
 } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
 
-import IllustrationImg from '../../assets/illustration.png'
-import { styles } from './styles'
+import { useAuth } from '../../hooks/auth';
+
+import IllustrationImg from '../../assets/illustration.png';
+import { theme } from '../../global/styles/theme';
+import { styles } from './styles';
 
 import { ButtonIcon } from '../../components/ButtonIcon';
 import { Background } from '../../components/Background';
 
-export function SignIn() {
-  const navigation = useNavigation();
+export function SignIn(){
+  const { loading, signIn } = useAuth();
 
-  function handleSignIn() {
-    navigation.navigate('Home');
+  async function handleSignIn() {
+    try {
+      await signIn();
+    }catch (error) {
+      Alert.alert(error);
+    }
   }
 
-  return (
+  return(
     <Background>
-      <View style={styles.container}>
-        <Image
-          source={ IllustrationImg } 
-          style={styles.image}
+      <View style={styles.container}>     
+        <Image 
+          source={IllustrationImg} 
+          style={styles.image} 
           resizeMode="stretch"
         />
 
         <View style={styles.content}>
           <Text style={styles.title}>
             Conecte-se {'\n'}
-            e organize suas  {'\n'}
+            e organize suas {'\n'} 
             jogatinas
           </Text>
 
           <Text style={styles.subtitle}>
-            Crie grupos para jogar seus games {'\n'}
+            Crie grupos para jogar seus games {'\n'} 
             favoritos com seus amigos
           </Text>
 
-          <ButtonIcon
-            title="Entrar com Discord" 
-            onPress={handleSignIn}
-          />
+          {
+            loading ? <ActivityIndicator color={theme.colors.primary} /> :
+            <ButtonIcon 
+              title="Entrar com Discord"
+              onPress={handleSignIn}
+            />  
+          }                             
         </View>
       </View>
     </Background>
